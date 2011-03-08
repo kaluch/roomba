7c63d9faae1d7bcad284668d94b0ad3b7278af1d
Lucas Souchet <kalu.souch@gmail.com>
2011-03-08 21:17:16 +0100

    update forme.java

--- Projet Roomba/src/roomba/Forme.java
+++ Projet Roomba/src/roomba/Forme.java
@@ -1,5 +1,16 @@
 package roomba;
 
+import java.util.ArrayList;
+
 public class Forme {
 
+	/*
+	 * ATTRIBUTS
+	 */
+	
+	protected int nbDeCote;
+	protected ArrayList<Integer> dimensions;
+	protected Posture posture;
+	
+	
 }
