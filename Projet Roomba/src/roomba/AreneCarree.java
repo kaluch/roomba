18d6383df28d8679e999b9a477166aaba3c6f347
Lucas Souchet <souchet@pc160.polytech.upmc.fr>
2011-03-09 13:54:20 +0100

    synchronisation

--- Projet Roomba/src/roomba/AreneCarree.java
+++ Projet Roomba/src/roomba/AreneCarree.java
@@ -3,7 +3,7 @@ package roomba;
 public class AreneCarree extends Arene {
 
 	AreneCarree() {
-	//Initialise un carré de 4m de coté
+	//Initialise un carre de 4m de cote
 		super(4);
 		for(int i=0;i<4;i++)
 			this.getDimensions().set(i,new Integer(4));
